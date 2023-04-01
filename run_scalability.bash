90b4fc21043722f9256f4abf5e8b1d95517fe0e7
Aleksei Sholokhov <aksh@uw.edu>
2023-03-31 18:37:51 -0700

    Change scaling experiment to add different etas

--- run_scalability.bash
+++ run_scalability.bash
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-#SBATCH --job-name="scaling"
+#SBATCH --job-name="scaling_jones"
 #SBATCH --account=amath
 #SBATCH --partition=ckpt
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
 #SBATCH --time=3:55:00
-#SBATCH --array=1-700%100
+#SBATCH --array=1-5000%50
 #SBATCH --output=logs/slurm_scaling/%A_%a.out
 #SBATCH --mem=8G
 
+ic="jones_bic"
 source ~/.bashrc
 conda activate compute
 cd ~/storage/repos/msr3-paper
 
-time python scalability.py  --trial ${SLURM_ARRAY_TASK_ID}
+time python scalability.py  --trial ${SLURM_ARRAY_TASK_ID} --ic $ic --experiment_name "scaling_{$ic}"
 
 
--- scalability.py
+++ scalability.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 import argparse
 from pathlib import Path
 import pickle
@@ -7,7 +8,7 @@ from sklearn.utils.fixes import loguniform
 from sklearn.model_selection import RandomizedSearchCV
 from sklearn.metrics import confusion_matrix
 
-from pysr3.lme.problems import LMEProblem, FIXED_RANDOM
+from pysr3.lme.problems import LMEProblem, FIXED_RANDOM, LMEStratifiedShuffleSplit
 from pysr3.lme.models import L1LmeModelSR3, L1LmeModel
 
 import time
@@ -17,24 +18,31 @@ parser.add_argument('--trial', type=int, default=1, help='Each "trial" represent
 parser.add_argument('--base_folder', type=str, default=".", help='Path to the base folder (where this file is).')
 parser.add_argument('--seed', type=int, default=42, help='Random seed')
 parser.add_argument('--experiment_name', type=str, default="scaling", help='Experiment name')
+parser.add_argument('--n_gridsearch_lambda', type=int, default=10)
+parser.add_argument('--ic', type=str, default="jones_bic")
 args = parser.parse_args()
 
 base_folder = Path(args.base_folder)
 
+possible_etas = [1e-2, 5e-2, 1e-1, 5e-1, 1e0, 5e0, 1e1, 5e1]
 problem_multipliers = [1, 2, 5, 10, 20, 50, 100]
 
 if __name__ == "__main__":
-    if args.trial < 700:
-        algo = "msr3-fast"
-    elif 700 <= args.trial < 1400:
-        algo = "pgd"
-    elif 1400 <= args.trial < 2100:
-        algo = "pgd_fixed"
+    multiplier = problem_multipliers[args.trial // 1000]
+    trial = args.trial % 100
+
+    if (args.trial % 1000) < 800:
+        algo = "MSR3-Fast"
+        eta = possible_etas[(args.trial % 1000) // 100]
+        model = L1LmeModelSR3(practical=True, tol_solver=1e-4 * multiplier, tol_oracle=1e-5 * multiplier, ell=eta)
+    elif 800 <= (args.trial % 1000) < 900:
+        algo = "PGD-LineSearch"
+        model = L1LmeModel(tol_solver=1e-4 * multiplier, stepping='line-search')
+    elif 900 <= (args.trial % 1000) < 1000:
+        algo = "PGD"
+        model = L1LmeModel(tol_solver=1e-4 * multiplier, stepping='fixed', fixed_step_len=1e-3)
     else:
         raise ValueError(f"Unknown trial num")
-
-    trial = (args.trial % 700) % 100
-    multiplier = problem_multipliers[(args.trial % 700) // 100]
 
     experiment_folder = base_folder / "results" / f"{args.experiment_name}"
     logs_folder = experiment_folder / "logs"
@@ -66,21 +74,32 @@ if __name__ == "__main__":
     # You can also convert it to pandas dataframe if you'd like.
     pandas_dataframe = problem.to_dataframe()
 
-    # We use SR3-empowered LASSO model, but many other popular models are also available.
-    # See the glossary of models for more details.
-    if algo == "msr3-fast":
-        model = L1LmeModelSR3(practical=True, tol_solver=1e-4 * multiplier, tol_oracle=1e-5 * multiplier, ell=20, lam=1e-3)
-    elif algo == "pgd":
-        model = L1LmeModel(tol_solver=1e-4 * multiplier, lam=1, stepping='line-search')
-    elif algo == "pgd_fixed":
-        model = L1LmeModel(tol_solver=1e-4 * multiplier, lam=1, stepping='fixed', fixed_step_len=1e-3)
-    else:
-        raise ValueError(f"Unknown algo: {algo}")
-
+    params = {
+        "lam": loguniform(1e-4, 1e2),
+    }
+    # We use standard functionality of sklearn to perform grid-search.
+    selector = RandomizedSearchCV(estimator=model,
+                                  param_distributions=params,
+                                  n_iter=args.n_gridsearch_lambda,  # number of points from parameters space to sample
+                                  # the class below implements CV-splits for LME models
+                                  cv=LMEStratifiedShuffleSplit(n_splits=2, test_size=0.5,
+                                                               random_state=args.seed,
+                                                               columns_labels=columns_labels),
+                                  # The function below will evaluate the information criterion
+                                  # on the test-sets during cross-validation.
+                                  # We use cAIC from Vaida, but other options (BIC, Muller's IC) are also available
+                                  scoring=lambda clf, x, y: -clf.get_information_criterion(x,
+                                                                                           y,
+                                                                                           columns_labels=columns_labels,
+                                                                                           ic=args.ic),
+                                  random_state=args.seed,
+                                  n_jobs=1
+                                  )
     start = time.time()
-    model.fit(x, y, columns_labels=columns_labels)
+    selector.fit(x, y, columns_labels=columns_labels)
     end = time.time()
-    best_model = model
+    exec_time = (end - start) / args.n_gridsearch_lambda
+    best_model = selector.best_estimator_
 
     maybe_beta = best_model.coef_["beta"]
     maybe_gamma = best_model.coef_["gamma"]
@@ -99,12 +118,26 @@ if __name__ == "__main__":
         "ffp": ffp,
         "ffn": ffn,
         "ftp": ftp,
-        "time": end - start
+        "rtn": rtn,
+        'rfp': rfp,
+        'rfn': rfn,
+        'rtp': rtp,
+        "time": exec_time
     }
     with open(logs_folder / f"log_{args.trial}.pickle", "wb") as f:
         pickle.dump(logs, f)
 
 
+def process_results(logs_folder: Path):
+    logs = []
+    for file in logs_folder.iterdir():
+        trial_num = file.name.split(".")[0].split("_")[1]
+        with open(file, 'rb') as f:
+            logs.append({"trial": int(trial_num), **pickle.load(f)})
+    data = pd.DataFrame.from_records(logs).sort_values(by='trial')
+    data['n_features'] = data['ftn'] + data['ffp'] + data['ffn'] + data['ftp']
+    data['accuracy'] = (data["ftp"] + data["ftn"]) / (data['n_features'])
+    data['time'] = data['time'].round(2)
 
-
-
+    groups = data[['algo', 'n_features', 'time']].pivot_table(index='n_features', columns='algo', values='time')
+    pass
